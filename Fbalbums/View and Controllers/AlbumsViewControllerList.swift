2e46c7535f7bfe5bc6d28f71885e69c146dd1d19
win-design <smartt24@gmail.com>
2018-01-02 20:30:32 +0000

    Clean code

--- Fbalbums/View and Controllers/AlbumsViewControllerList.swift
+++ Fbalbums/View and Controllers/AlbumsViewControllerList.swift
@@ -45,49 +45,7 @@ class AlbumsViewControllerList: UITableViewController {
         performSegue(withIdentifier: "showPhotos", sender: album)
 
     }
-    // partie fun for loding albumes
     
-    func loadFirstAlbums() {
-        //isLoadingAlbums = true
-//        StarWarsSpecies.getSpecies { result in
-//            if let error = result.error {
-//                // TODO: improved error handling
-//                self.isLoadingAlbums = false
-//                let alert = UIAlertController(title: "Error", message: "Could not load first species :( \(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.alert)
-//                alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
-//                self.present(alert, animated: true, completion: nil)
-//            }
-//            let speciesWrapper = result.value
-//           // self.addSpeciesFromWrapper(speciesWrapper)
-//            self.isLoadingAlbums = false
-//            self.tableview?.reloadData()
-//        }
-    }
-    // loding seconde
-    func loadMoreAlbums() {
-//        self.isLoadingSpecies = true
-//        if let species = self.species,
-//            let wrapper = self.speciesWrapper,
-//            let totalSpeciesCount = wrapper.count,
-//            species.count < totalSpeciesCount {
-//            // there are more species out there!
-//            StarWarsSpecies.getMoreSpecies(speciesWrapper) { result in
-//                if let error = result.error {
-//                    self.isLoadingSpecies = false
-//                    let alert = UIAlertController(title: "Error", message: "Could not load more species :( \(error.localizedDescription)", preferredStyle: UIAlertControllerStyle.alert)
-//                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
-//                    self.present(alert, animated: true, completion: nil)
-//                }
-//                let moreWrapper = result.value
-//                self.addSpeciesFromWrapper(moreWrapper)
-//                self.isLoadingSpecies = false
-//                self.tableview?.reloadData()
-//            }
-//        }
-        
-    }
-    //
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return albums.count
@@ -99,17 +57,6 @@ class AlbumsViewControllerList: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListPhotoCell", for: indexPath) as! ListPhotoCell
         cell.descLabel.text = album.name
         cell.photo.sd_setImage(with: album.imageUrl)
-        // See if we need to load more species
-        /*
-        let rowsToLoadFromBottom = 5;
-        let rowsLoaded = albums.count
-        if (!self.isLoadingAlbums && (indexPath.row >= (rowsLoaded - rowsToLoadFromBottom))) {
-            //let totalRows = self.speciesWrapper?.count ?? 0
-            let remainingSpeciesToLoad = totalRows - rowsLoaded;
-            if (remainingSpeciesToLoad > 0) {
-                self.loadMoreAlbums()
-            }
- */
         return cell
         
     }
